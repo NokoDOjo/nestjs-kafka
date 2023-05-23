0b972b9457a0b082a77c24a5566e1998cc24bcb0
Benny4idps <benny@4idps.com>
2023-05-23 11:23:43 +0800

    initiate producer & consumer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KafkaModule } from './kafka/kafka.module';
 import { TestConsumer } from './test.consumer';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
--- src/kafka/consumer.service.ts
+++ src/kafka/consumer.service.ts
@@ -1,19 +1,41 @@
 import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 import {
   Consumer,
+  ConsumerConfig,
   ConsumerRunConfig,
   ConsumerSubscribeTopics,
   Kafka,
+  KafkaMessage,
 } from 'kafkajs';
 import { IConsumer } from './consumer.interface';
+import { ConfigService } from '@nestjs/config';
+import { KafkaJsConsumer } from './kafkajs.consumer';
 
+interface KafkaConsumerOptions {
+  topic: ConsumerSubscribeTopics;
+  config: ConsumerConfig;
+  onMessage: (message: KafkaMessage) => Promise<void>;
+}
 @Injectable()
 export class ConsumerService implements OnApplicationShutdown {
   private readonly consumers: IConsumer[] = [];
-  
-  constructor(
 
-  ) {
+  constructor(private readonly configService: ConfigService) {}
 
+  async consume({ topic, config, onMessage }: KafkaConsumerOptions) {
+    const consumer = new KafkaJsConsumer(
+      topic,
+      config,
+      this.configService.get('KAFKA_BROKER'),
+    );
+    await consumer.connect();
+    await consumer.consume(onMessage);
+    this.consumers.push(consumer);
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    for (const consumer of this.consumers) {
+      await consumer.disconnect();
+    }
   }
 }
